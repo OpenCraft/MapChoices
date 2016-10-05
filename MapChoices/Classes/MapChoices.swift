01eb00d7e581619d01e9321129f2e42d961b2705
Cleber <cleber.neto@ilegra.com>
2016-10-05 02:56:23 -0300

    Added MapChoices.swift to project

---
+++ MapChoices/Classes/MapChoices.swift
@@ -0,0 +1,53 @@
+import CoreLocation
+import UIKit
+
+class MapChoices {
+    
+    private static let WAZE = "Waze"
+    private static let GOOGLEMAPS = "Google Maps"
+    private static let APPLEMAPS = "Apple Maps"
+    
+    private struct MapChoice {
+        var name: String
+        var url: NSURL
+    }
+    
+    internal static func presentMapChoicesInViewController(viewController: UIViewController, coordinate: CLLocationCoordinate2D) {
+        let choicesAlert = UIAlertController(title: "Mapas", message: "Selecione uma opção de navegação", preferredStyle: .ActionSheet)
+        
+        getMapChoices(coordinate).forEach { choice in
+            choicesAlert.addAction(UIAlertAction(title: choice.name, style: .Default, handler: { _ in
+                UIApplication.sharedApplication().openURL(choice.url)
+            }))
+        }
+        
+        viewController.presentViewController(choicesAlert, animated: true, completion: nil)
+    }
+    
+    private static func getMapChoices(coordinate: CLLocationCoordinate2D) -> [MapChoice] {
+        
+        var mapChoices = [MapChoice]()
+        
+        if let appleMapsURL = NSURL(string: "http://maps.apple.com/?daddr=\(String(coordinate.latitude)),\(String(coordinate.longitude))") {
+            if UIApplication.sharedApplication().canOpenURL(appleMapsURL) {
+                mapChoices.append(MapChoice(name: APPLEMAPS, url: appleMapsURL))
+            }
+        }
+        
+        if let wazeURL = NSURL(string: "waze://?ll="+String(coordinate.latitude)+","+String(coordinate.longitude)+"&navigate=yes") {
+            if UIApplication.sharedApplication().canOpenURL(wazeURL) {
+                mapChoices.append(MapChoice(name: WAZE, url: wazeURL))
+            }
+        }
+        
+        if let googleMapsURL = NSURL(string: "comgooglemaps://?daddress="+String(coordinate.latitude)+","+String(coordinate.longitude)) {
+            if UIApplication.sharedApplication().canOpenURL(googleMapsURL) {
+                mapChoices.append(MapChoice(name: GOOGLEMAPS, url: googleMapsURL))
+            }
+        }
+        
+        return mapChoices
+        
+    }
+    
+}
