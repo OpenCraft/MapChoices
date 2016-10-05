c9e43fb3bedc4181ba20c1cf8c4156835c5546b9
Cleber <cleber.neto@ilegra.com>
2016-10-05 02:56:47 -0300

    Added example to git

---
+++ Example/Pods/Target Support Files/MapChoices/MapChoices-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MapChoices : NSObject
+@end
+@implementation PodsDummy_MapChoices
+@end
---
+++ Example/Pods/Target Support Files/MapChoices/MapChoices-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MapChoicesVersionNumber;
+FOUNDATION_EXPORT const unsigned char MapChoicesVersionString[];
+
