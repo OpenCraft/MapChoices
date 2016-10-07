ab91e7a914c614e122273dcc251f37fad2d961e9
Cleber <cleber.neto@ilegra.com>
2016-10-07 10:21:48 -0300

    Added support for Carthage

---
+++ MapChoices-iOS/MapChoices-iOS/MapChoices-iOS.h
@@ -0,0 +1,19 @@
+//
+//  MapChoices-iOS.h
+//  MapChoices-iOS
+//
+//  Created by Cleber Henriques on 10/7/16.
+//  Copyright © 2016 Cleber Henriques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapChoices-iOS.
+FOUNDATION_EXPORT double MapChoices_iOSVersionNumber;
+
+//! Project version string for MapChoices-iOS.
+FOUNDATION_EXPORT const unsigned char MapChoices_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapChoices_iOS/PublicHeader.h>
+
+
