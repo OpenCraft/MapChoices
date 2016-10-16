4755dc8a3faedb07e8d1dbd3109c4b857af2ac9d
Henrique Morbin <morbin_@hotmail.com>
2016-10-16 19:56:46 -0200

    Example refactor

--- Example/MapChoices/ViewController.swift
+++ Example/MapChoices/ViewController.swift
@@ -11,21 +11,11 @@ import CoreLocation
 import MapChoices
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func showMapsDidTouch(sender: AnyObject) {
         let testLocation = CLLocationCoordinate2D(latitude: 37.3314187, longitude: -122.0325976)
         
-        MapChoices.presentMapChoicesInViewController(self, coordinate: testLocation)
+        MapChoices.presentMapChoices(inViewController: self, coordinate: testLocation)
     }
 
 }
